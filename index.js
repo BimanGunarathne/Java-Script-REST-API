3d5209820b044d9febb82a548ac92087ae9d2376
Biman Gunarathne <bimangunarathne@outlook.com>
2023-09-16 06:54:30 +0530

    Commiting

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 // Bring in the express server and create application
 let express = require('express');
 let app = express();
+let cakeRepo = require("./repos/cakeRepo");
 
 // Use the express Router object
 let router = express.Router();
-let cakes = [
-    { "id": 1, "name": "Coffe" },
-    { "id": 2, "name": "Ribon" },
-    { "id": 3, "name": "Butter" }
-]
+
 
 // Create GET to return a list of all cakes
 router.get('/', function(req, res, next) {
-    res.status(200).json({
-        status: 200,
-        statusText: "OK",
-        message: "All cakes retrieved.",
-        data: cakes
-    });
+    cakeRepo.get(function(data) {
+        res.status(200).json({
+            status: 200,
+            statusText: "OK",
+            message: "All cakes retrieved.",
+            data
+        });
+    }, function(err) {
+        next(err);
+    })
 });
 
 // Configure router so all routes are prefixed with /api/v1
